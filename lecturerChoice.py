786c7ed5cbc93c26e0783f09c692456e8e6b96a6
MajorTomaso <majortomasso@gmail.com>
2019-03-27 14:10:00 +0000

    Made menu

--- lecturerChoice.py
+++ lecturerChoice.py
@@ -9,23 +9,32 @@ class lecturerChoice(Frame):
         self.createButtons()
 
     def createPage(self):
-    	self.txtDisplay = Text(self, height=10, width=50)
+    	self.txtDisplay = Text(self, height=3, width=50)
     	self.txtDisplay.tag_configure('boldfont', font=('MS',8, 'bold'))
 
-    	self.txtDisplay.insert(END, "\t\tChoose a test to view results")
+    	self.txtDisplay.insert(END, "\t\t      Menu")
 
 
     	self.txtDisplay['state'] = DISABLED
     	self.txtDisplay.pack()
 
     def createButtons(self):
-    	button_lecturer = Button(self, text="Summative Assessment", font = ('MS', 8, 'bold'))
+    	button_lecturer = Button(self, text="View Summative Results", font = ('MS', 8, 'bold'))
     	button_lecturer['command'] = self.Summative
     	button_lecturer.pack()
+    	
 
-    	button_student = Button(self,text="Formative Assessment", font=('MS',8,'bold'))
+    	button_student = Button(self,text="View Formative Results", font=('MS',8,'bold'))
     	button_student['command'] = self.Formative
     	button_student.pack()
+
+    	button_create = Button(self, text="Create Assessment", font = ('MS', 8, 'bold'))
+    	button_create['command'] = self.CreateAssessment
+    	button_create.pack()
+
+    	button_modify= Button(self, text="Modify Assessment", font = ('MS', 8, 'bold'))
+    	button_modify['command'] = self.ModifyAssessment
+    	button_modify.pack()
     def Summative(self):
     	root.destroy()
     	import ViewSummativeResults
@@ -34,10 +43,16 @@ class lecturerChoice(Frame):
     def Formative(self):
     	root.destroy()
     	import ViewFormativeResults
+    def CreateAssessment(self):
+        root.destroy()
+        import CreateAssessment
 
+    def ModifyAssessment(self):
+        root.destroy()
+        import chooseModify
 
 #Main
 root = Tk()
-root.title("Choose a test")
+root.title("Lecturer Menu")
 app = lecturerChoice(root)
 root.mainloop()
